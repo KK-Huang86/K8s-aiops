a41294ec17c0e865f2bd68a4130aa7176d0b5625
KK <iankk0806@gmail.com>
2026-05-03 16:44:48 +0800

    feat: setting Thanos Sidecar

--- prometheus.tf
+++ prometheus.tf
@@ -17,5 +17,35 @@ resource "helm_release" "kube_prometheus_stack" {
     value = "true"
   }
 
-  depends_on = [local_file.kubeconfig]
+  # Thanos Sidecar：注入到 Prometheus Pod 裡
+  set {
+    name  = "prometheus.prometheusSpec.thanos.image"
+    value = "quay.io/thanos/thanos:v0.37.2"
+  }
+
+  set {
+    name  = "prometheus.prometheusSpec.thanos.objectStorageConfig.name"
+    value = "thanos-objstore-secret"
+  }
+
+  set {
+    name  = "prometheus.prometheusSpec.thanos.objectStorageConfig.key"
+    value = "objstore.yml"
+  }
+
+  # Thanos 需要 block duration 固定，才能正確上傳到 S3
+  set {
+    name  = "prometheus.prometheusSpec.retention"
+    value = "5h"
+  }
+
+  set {
+    name  = "prometheus.prometheusSpec.retentionSize"
+    value = "1GB"
+  }
+
+  depends_on = [
+    local_file.kubeconfig,
+    kubernetes_secret.thanos_objstore,
+  ]
 }
